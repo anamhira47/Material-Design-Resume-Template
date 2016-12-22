a0898b6d9d74a88c46aefefbec481ea18810668c
anamhira47 <hira.anam49@gmail.com>
2016-12-21 16:45:39 -0800

    Explained the index.php more

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
     <head>
         <meta charset="utf-8">
 
-        <!-- TITLE OF SITE -->
+        <!-- TITLE OF SITE
+        Deploy this material Design HTML template to your website
+        -->
         <title> Material Design Resume</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Materialize portfolio one page template, Using for personal website." />
